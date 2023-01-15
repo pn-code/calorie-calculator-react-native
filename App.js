d457501d7f33089f0eb520fa89285ad7e086109e
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-15 14:39:53 -0800

    Added core components to App

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [weight, setWeight] = useState();
+
+	return (
+		<View style={styles.container}>
+			{/* Header */}
+			<View>
+				<Text style={styles.header}>Calculator</Text>
+			</View>
+			{/* Form */}
+			<View>
+				<TextInput
+					type="numbers"
+					placeholder="Input Weight (lb)"
+				></TextInput>
+				<Button title="Calculate"></Button>
+			</View>
+      {/* Text Output */}
+      <View>
+        <Text>Daily Calorie Expenditure: {`<calories>`}</Text>
+        <Text>To Lose Weight: {`<calories>`} - 300-500 calories.</Text>
+        <Text>To Gain Weight: {`<calories>`} + 200-300 calories.</Text>
+        <Text>To Maintain Weight: {`<calories>`} calories</Text>
+      </View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	header: {
+		backgroundColor: "red",
+		fontSize: "42",
+	},
 });
